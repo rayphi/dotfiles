512d21fd1e5bd31eaefe3c6ab0a582354368f1c7
Raphael Tweitmann <r.tweity@googlemail.com>
2016-03-11 09:42:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,33 @@
-#!/bin/sh
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 if [ -f ~/.bashrc ]; then
     {
-        echo "if [ -f ${PWD}/.bashrc ]; then";
-        echo "    . ${PWD}/.bashrc";
+        echo "if [ -f ${DIR}/.bashrc ]; then";
+        echo "    . ${DIR}/.bashrc";
         echo "fi";
     } >> ~/.bashrc
 else
-    ln -s ${PWD}/.bashrc ~/.bashrc
+    ln -s ${DIR}/.bashrc ~/.bashrc
 fi
 
 if [ -f ~/.bash_aliases ]; then
-	{
-        echo "if [ -f ${PWD}/.bash_aliases ]; then";
-        echo "    . ${PWD}/.bash_aliases";
+        {
+        echo "if [ -f ${DIR}/.bash_aliases ]; then";
+        echo "    . ${DIR}/.bash_aliases";
         echo "fi";
     } >> ~/.bash_aliases
 else
-    ln -s ${PWD}/.bash_aliases ~/.bash_aliases
+    ln -s ${DIR}/.bash_aliases ~/.bash_aliases
 fi
 
 if [ -f ~/.bash_git ]; then
     {
-        echo "if [ -f ${PWD}/.bash_git ]; then";
-        echo "    . ${PWD}/.bash_git";
+        echo "if [ -f ${DIR}/.bash_git ]; then";
+        echo "    . ${DIR}/.bash_git";
         echo "fi";
 } >> ~/.bash_git
 else
-	ln -s ${PWD}/.bash_git ~/.bash_git
+        ln -s ${DIR}/.bash_git ~/.bash_git
 fi
